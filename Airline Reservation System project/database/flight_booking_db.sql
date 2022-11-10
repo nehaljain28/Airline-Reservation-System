d936de8bd0fef7c7c7f6fd788d6616dc75b262ed
Nehal <nehalcoding@gmail.com>
2022-11-10 23:26:02 +0530

    first

---
+++ Airline Reservation System project/database/flight_booking_db.sql
@@ -0,0 +1,169 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+
+CREATE TABLE `airlines_list` (
+  `id` int(30) NOT NULL,
+  `airlines` text NOT NULL,
+  `logo_path` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+INSERT INTO `airlines_list` (`id`, `airlines`, `logo_path`) VALUES
+(1, 'AirIndia', '1600999080_kisspng-flight-indonesia-airasia-airasia-japan-airline-tic-asia-5abad146966736.8321896415221927106161.jpg'),
+(2, 'IndiGo', '1600999200_Philippine-Airlines-Logo.jpg'),
+(3, 'Vistara', '1600999200_43cada0008538e3c1a1f4675e5a7aabe.jpeg');
+
+-- --------------------------------------------------------
+
+
+
+CREATE TABLE `airport_list` (
+  `id` int(30) NOT NULL,
+  `airport` text NOT NULL,
+  `location` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+INSERT INTO `airport_list` (`id`, `airport`, `location`) VALUES
+(1, 'NAIA', 'Metro Manila'),
+(2, 'Beijing Capital International Airport', 'Chaoyang-Shunyi, Beijing'),
+(3, 'Los Angeles International Airport', 'Los Angeles, California'),
+(4, 'Dubai International Airport', 'Garhoud, Dubai'),
+(5, 'Mactan-Cebu Airport', 'Cebu');
+
+-- --------------------------------------------------------
+
+
+
+CREATE TABLE `booked_flight` (
+  `id` int(30) NOT NULL,
+  `flight_id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `address` text NOT NULL,
+  `contact` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+
+
+CREATE TABLE `flight_list` (
+  `id` int(30) NOT NULL,
+  `airline_id` int(30) NOT NULL,
+  `plane_no` text NOT NULL,
+  `departure_airport_id` int(30) NOT NULL,
+  `arrival_airport_id` int(30) NOT NULL,
+  `departure_datetime` datetime NOT NULL,
+  `arrival_datetime` datetime NOT NULL,
+  `seats` int(10) NOT NULL DEFAULT 0,
+  `price` double NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+INSERT INTO `flight_list` (`id`, `airline_id`, `plane_no`, `departure_airport_id`, `arrival_airport_id`, `departure_datetime`, `arrival_datetime`, `seats`, `price`, `date_created`) VALUES
+(4, 3, 'CEB10023', 1, 5, '2021-03-20 01:00:00', '2021-03-20 02:00:00', 100, 2500, '2021-02-25 14:50:47');
+
+-- --------------------------------------------------------
+
+
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `cover_img` text NOT NULL,
+  `about_content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `cover_img`, `about_content`) VALUES
+(1, 'Online Flight Booking System', 'info@sample.comm', '+6948 8542 623', '1615490820_banner.jpg', '&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;span style=&quot;background: transparent; position: relative; font-size: 14px;&quot;&gt;&lt;span style=&quot;font-size:28px;background: transparent; position: relative;&quot;&gt;&lt;b style=&quot;margin: 0px; padding: 0px; color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; text-align: justify;&quot;&gt;Lorem Ipsum&lt;/b&gt;&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-weight: 400; text-align: justify;&quot;&gt;&amp;nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&rsquo;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.&lt;/span&gt;&lt;br&gt;&lt;/span&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;&lt;/p&gt;');
+
+-- --------------------------------------------------------
+
+
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `doctor_id` int(30) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `address` text NOT NULL,
+  `contact` text NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1=admin , 2 = doctor,3=patient'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+INSERT INTO `users` (`id`, `doctor_id`, `name`, `address`, `contact`, `username`, `password`, `type`) VALUES
+(1, 0, 'Administrator', '', '', 'admin', 'admin123', 1),
+(7, 0, 'George Wilson', 'Sample Only', '+18456-5455-55', 'gwilson@sample.com', 'd40242fb23c45206fadee4e2418f274f', 3),
+(9, 2, 'DR.James Smith, M.D.', 'Sample Clinic Address', '+1456 554 55623', 'jsmith@sample.com', 'jsmith123', 2),
+(10, 3, 'DR.Claire Blake, M.D.', 'Sample Only', '+5465 555 623', 'cblake@sample.com', 'blake123', 2),
+(11, 0, 'Sample Only', 'Sample', '+5465 546 4556', 'sample@sample.com', '4e91b1cbe42b5c884de47d4c7fda0555', 3),
+(15, 9, 'DR.Sample Doctor, M.D.', 'Sample Address', '+1235 456 623', 'sample2@sample.com', 'sample123', 2);
+
+
+ALTER TABLE `airlines_list`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `airport_list`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `booked_flight`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `flight_list`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `airlines_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+
+ALTER TABLE `airport_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+
+ALTER TABLE `booked_flight`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+
+ALTER TABLE `flight_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
